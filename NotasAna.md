a7fbc56fae55d993c3507727ae6beb30cf3b0a68
Ana Margarida Almeida <ana.margarida.almeida@tecnico.ulisboa.pt>
2024-05-05 17:10:48 +0100

    Implement random objects position

--- NotasAna.md
+++ NotasAna.md
@@ -11,7 +11,15 @@
 ## Dúvidas
 
 - [ ] Os cantos do contentor estão ok?
-- [ ] Posição dos objetos deve ser aleatória?
+- [X] Posição dos objetos deve ser aleatória?
+  - Acho que sim, ripzão :)
 
 ## TODO
+
+- [X] Pôr as cargas a serem geradas em posições aleatórias
+- [X] Criar função que desenhe as figuras todas
+  - Ao invés de andar a chamar 6/7 funções no `createScene`
+- [ ] Pôr as variaveis todas em português ou inglês
+  - A mistura é que não dá
+- [ ] Estudar se não há forma de optimizar isto
 
--- src/js/grua.js
+++ src/js/grua.js
@@ -8,14 +8,25 @@ var materialBase, materialParedes, materialCube, materialTorus, materialDodecaed
 
 var container;
 
+var larguraContainer = 10;
+var alturaContainer = 10;
+var comprimentoContainer = 20;
+
+var ladoCubo = 2;
+
+var larguraParallelpiped = 2;
+var alturaParallelpiped = 5;
+var comprimentoParallelpiped = 2;
+
+var raioTorus = 3;
+var raioTubo = 1;
+
+var raio = 5
+
 function createContainer(x,y,z){
     'use strict';
 
     container = new THREE.Object3D();
-
-    var larguraContainer = 10;
-    var alturaContainer = 10;
-    var comprimentoContainer = 20;
 
     materialBase = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
     materialParedes = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
@@ -60,7 +71,7 @@ function createCube(x,y,z){
 
     materialCube = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-    var cubeGeometry = new THREE.BoxGeometry(2,2,2);
+    var cubeGeometry = new THREE.BoxGeometry(ladoCubo,ladoCubo,ladoCubo);
     var cube = new THREE.Mesh(cubeGeometry, materialCube);
     cube.position.set(x,y,z);
     scene.add(cube);
@@ -70,9 +81,6 @@ function createTorus(x,y,z){
     'use strict';
 
     materialTorus = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-    var raioTorus = 3;
-    var raioTubo = 1;
 
     var torusGeometry = new THREE.TorusGeometry(raioTorus, raioTubo);
     var torus = new THREE.Mesh(torusGeometry, materialTorus);
@@ -87,9 +95,6 @@ function createTorusKnot(x,y,z){
 
     materialTorusKnot = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-    var raioTorus = 15;
-    var raioTubo = 5;
-
     var torusGeometry = new THREE.TorusKnotGeometry(raioTorus, raioTubo);
     var torus = new THREE.Mesh(torusGeometry, materialTorusKnot);
     torus.position.set(x,y,z);
@@ -102,8 +107,6 @@ function createDodecahedron(x,y,z){
 
     materialDodecaedro = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-    var raio = 5
-
     var dodecahedronGeometry = new THREE.DodecahedronGeometry(raio);
     var dodecahedron = new THREE.Mesh(dodecahedronGeometry, materialDodecaedro);
     dodecahedron.position.set(x,y,z);
@@ -114,8 +117,6 @@ function createIsocahedron(x,y,z){
     'use strict';
 
     materialIsocahedron = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-    var raio = 5
 
     var isocahedronGeometry = new THREE.IcosahedronGeometry(raio);
     var isocahedron = new THREE.Mesh(isocahedronGeometry, materialIsocahedron);
@@ -129,15 +130,78 @@ function createParallelpiped(x,y,z){
 
     materialParallelpiped = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
-    var larguraParallelpiped = 2;
-    var alturaParallelpiped = 5;
-    var comprimentoParallelpiped = 2;
-
     var parallelpipedGeometry = new THREE.BoxGeometry(larguraParallelpiped,alturaParallelpiped,comprimentoParallelpiped);
     var parallelpiped = new THREE.Mesh(parallelpipedGeometry, materialParallelpiped);
     parallelpiped.position.set(x,y,z);
     scene.add(parallelpiped);
 }
+
+function createLoads(){
+    let i = 0;
+    while (i < 6) {
+        let x = Math.random() * 100-50;
+        let y = 0;
+        let z = Math.random() * 100-50;
+        
+        switch (i) {
+            case 0:
+                if(!verificarColisao(x,y,z, ladoCubo, ladoCubo, ladoCubo)){
+                    createCube(x,y,z);
+                    i++;
+                }
+                break;
+            case 1:
+                if(!verificarColisao(x,y,z, raioTorus, raioTubo, raioTorus)){
+                    createTorus(x,y,z);
+                    i++;
+                }
+                break;
+            case 2:
+                if(!verificarColisao(x,y,z, raioTorus, raioTubo, raioTorus)){
+                    createTorusKnot(x,y,z);
+                    i++;
+                }
+                break;
+            case 3:
+                if(!verificarColisao(x,y,z, raio, raio, raio)){
+                    createDodecahedron(x,y,z);
+                    i++;
+                }
+                break;
+            case 4:
+                if(!verificarColisao(x,y,z, raio, raio, raio)){
+                    createIsocahedron(x,y,z);
+                    i++;
+                }
+                break;
+            case 5:
+                if(!verificarColisao(x,y,z, larguraParallelpiped, alturaParallelpiped, comprimentoParallelpiped)){
+                    createParallelpiped(x,y,z);
+                    i++;
+                }
+                break;
+            default:
+                console.log("Erro");
+        }
+    }
+}
+
+function verificarColisao(x, y, z, largura, altura, comprimento) {
+
+    let novaBoundingBox = new THREE.Box3(
+        new THREE.Vector3(x-largura, y-altura, z-comprimento),
+        new THREE.Vector3(x + largura, y + altura, z+comprimento)
+    );
+
+    for (let objeto of scene.children) {
+            let boundingBox = new THREE.Box3().setFromObject(objeto, true);
+            if (novaBoundingBox.intersectsBox(boundingBox)) {
+                return true;
+            }
+    }
+    return false;
+}
+
 
 function render() {
     'use strict';
@@ -163,14 +227,8 @@ function createScene() {
 
     scene.add(new THREE.AxesHelper(10));
 
-    // createContainer(0,0,0);
-    // createCube(0,0,0);
-    // createTorus(0,0,0);
-    // createTorusKnot(0,0,0);
-    // createDodecahedron(0,0,0);
-    // createIsocahedron(0,0,0);
-    createParallelpiped(0,0,0);
-    // TODO
+    createContainer(15,0,15);
+    createLoads();
 }
 
 function createCamera() {
