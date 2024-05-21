0295900d2212204f8ffcf6118c12b54ca1a54675
Ana Margarida Almeida <ana.margarida.almeida@tecnico.ulisboa.pt>
2024-05-21 13:17:35 +0100

    Create cubes instead of parametric figures

--- src_C/js/main-script.js
+++ src_C/js/main-script.js
@@ -3,6 +3,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import * as Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+import { ParametricGeometries } from "three/addons/geometries/ParametricGeometries.js";
 
 //////////////////////
 /* GLOBAL VARIABLES */
@@ -71,6 +73,80 @@ var firstRingHorizontalSpeed = 4,
     secondRingVerticalSpeed = 1,
     thirdRingVerticalSpeed = 1;
 
+//////////////////////////
+/* Parametric Geometry */
+//////////////////////////
+var numberOfParametricFunctions = 8;
+// FIXME - Implement better parametric functions
+// TODO - Maybe move this to a separate file
+var parametricFunctions = [
+    // Superfície paramétrica 1: Paraboloide hiperbólico
+    function (u, v, target) {
+        const x = Math.cos(u * Math.PI * 2) * Math.sin(v * Math.PI);
+        const y = Math.sin(u * Math.PI * 2) * Math.sin(v * Math.PI);
+        const z = Math.cos(v * Math.PI);
+        target.set(x, y, z)
+    },
+    // Superfície paramétrica 2: Hiperbolóide de uma folha
+    function (u, v, target) {
+        const x = Math.cos(u * Math.PI * 2);
+        const y = Math.sin(u * Math.PI * 2);
+        const z = v * 2 - 1; // v vai de 0 a 1, então mapeamos para -1 a 1
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 3: Cilindro helicoidal
+    function (u, v, target) {
+        const radius = 1 - v;
+        const x = radius * Math.cos(u * Math.PI * 2);
+        const y = radius * Math.sin(u * Math.PI * 2);
+        const z = v * 2 - 1;
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 4: Elipsóide
+    function (u, v, target) {
+        const x = v * Math.cos(u * Math.PI * 2);
+        const y = v * Math.sin(u * Math.PI * 2);
+        const z = v * v;
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 5: Cone
+    function (u, v, target) {
+        const a = 1;
+        const b = 1;
+        const c = 1;
+        const x = a * Math.cosh(v) * Math.cos(u * Math.PI * 2);
+        const y = b * Math.cosh(v) * Math.sin(u * Math.PI * 2);
+        const z = c * Math.sinh(v);
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 6: Faixa de Möbius
+    function (u, v, target) {
+        const a = 1;
+        const b = 0.1;
+        const x = a * Math.cos(u * Math.PI * 2);
+        const y = a * Math.sin(u * Math.PI * 2);
+        const z = b * u;
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 7: Catenoide
+    function (u, v, target) {
+        const x = u;
+        const y = v * Math.sin(u);
+        const z = v * Math.cos(u);
+        target.set(x, y, z);
+    },
+    // Superfície paramétrica 8: Plano torcido
+    function (u, v, target) {
+        const R = 1; // Raio maior
+        const r = 0.3; // Raio menor
+        const x = (R + r * Math.cos(v * Math.PI * 2)) * Math.cos(u * Math.PI * 2);
+        const y = (R + r * Math.cos(v * Math.PI * 2)) * Math.sin(u * Math.PI * 2);
+        const z = r * Math.sin(v * Math.PI * 2);
+        target.set(x, y, z);
+    }
+];
+var parametricGeometries = [];
+
 ////////////////////////
 /* CREATE MATERIAL(S) */
 ////////////////////////
@@ -112,9 +188,9 @@ function createCamera() {
         window.innerWidth / window.innerHeight,
         1,
         1000);
-    camera.position.x = 15;
+    camera.position.x = 0;
     camera.position.y = 0;
-    camera.position.z = 15;
+    camera.position.z = 20;
     camera.lookAt(scene.position);
 }
 
@@ -165,18 +241,48 @@ function createFirstRing(x, y, z) {
     'use strict';
 
     firstRing = createRing(x, y, z, firstRingInnerRadius, firstRingOuterRadius, firstRingHeight, materialFirstRing);
+    createParametrics(firstRing, firstRingInnerRadius, firstRingOuterRadius, secondRingHeight, parametricFunctions);
 }
 
 function createSecondRing(x, y, z) {
     'use strict';
 
     secondRing = createRing(x, y, z, secondRingInnerRadius, secondRingOuterRadius, secondRingHeight, materialSecondRing);
+    createParametrics(secondRing, secondRingInnerRadius, secondRingOuterRadius, secondRingHeight, parametricFunctions);
 }
 
 function createThirdRing(x, y, z) {
     'use strict';
 
     thirdRing = createRing(x, y, z, thirdRingInnerRadius, thirdRingOuterRadius, thirdRingHeight, materialThirdRing);
+    createParametrics(thirdRing, thirdRingInnerRadius, thirdRingOuterRadius, thirdRingHeight, parametricFunctions);
+}
+
+function createParametrics(ring, innerRadius, outerRadius, height, parametricFunctions) {
+    var numberOfParametricFunctions = parametricFunctions.length;
+
+    const radius = innerRadius + (outerRadius - innerRadius) / 2;
+
+    for (var i = 0; i < numberOfParametricFunctions; i++) {
+        const angle = i * (Math.PI / 4);
+        const x = radius * Math.cos(angle);
+        const z = radius * Math.sin(angle);
+        // const x = (2 / 3) * radius
+
+        // const parametricFunction = parametricFunctions[i];
+
+        // const geometry = new ParametricGeometry(parametricFunction, 1, 8);
+        const cubeSize = 1;
+        const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+        const material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
+        const surface = new THREE.Mesh(geometry, material);
+
+        surface.position.set(x, z, -cubeSize / 2);
+        ring.add(surface);
+        console.log("Added surface ", i, " to ring with angle ", ring.rotation.z);
+        ring.rotation.z += (Math.PI / 4);
+    }
+
 }
 
 //////////////////////
